408d355c89f08406181af34b4343867fdabe0626
Katherine Worley <kdesign216@yahoo.com>
2018-01-18 10:10:47 -0500

    added devise and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :recipes
 
-  root "recipes#index"
-  
+  root "recipes#index"  
 end
